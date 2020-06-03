18189946614c0dd8a724d41c26fa9c5840063d5c
Andrew-CC-Martin <amartin262@gmail.com>
2020-06-03 11:04:09 +1000

    add image loader, html generator, dist cleanup

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 // import ReactDOM from 'react-dom';
 
 // import App from './app/App';
- 
+
+// const App = () => <p>hello there</p>
+
 // ReactDOM.render(<App />, document.getElementById('react'));
 
 const node = document.getElementById('react')
-const textNode = document.createTextNode('hello world');
+const textNode = document.createTextNode('hello world')
+
 node.appendChild(textNode)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
-const path = require('path');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+/**
+ * Notes:
+ * - I haven't added a css loader, as I'm planning on just using styled components
+ * (though I'm not sure if that will work without a css loader)
+ * - Likewise for fonts (I may just add them in the html file, if I even add any)
+ * - Images are loaded but not modified. Webpack has a package (image-webpack-loader)
+ * which can be used to minify and optimise images
+ * Other features to consider adding:
+ *   - code splitting
+ *   -filename hashing (to ensure code updates result in client's cache being updated)
+ **/
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-};
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+}
